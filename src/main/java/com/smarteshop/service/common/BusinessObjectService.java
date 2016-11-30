e4db36f1b69631f33d9e14c79b656ba8019362d7
陈少钦 <chenshaoqin@chenshaoqindeMacBook-Pro.local>
2016-11-30 20:14:10 +0800

    init ...

---
+++ src/main/java/com/smarteshop/service/common/BusinessObjectService.java
@@ -0,0 +1,5 @@
+package com.smarteshop.service.common;
+
+public interface BusinessObjectService {
+
+}
---
+++ src/main/java/com/smarteshop/web/rest/common/AbstractBaseController.java
@@ -0,0 +1,9 @@
+package com.smarteshop.web.rest.common;
+
+import com.smarteshop.domain.AbstractAuditingEntity;
+
+public class AbstractBaseController  <E extends AbstractAuditingEntity>{
+
+
+
+}
