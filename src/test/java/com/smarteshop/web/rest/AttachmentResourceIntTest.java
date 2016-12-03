c143dcbb2299db99647240e4d6d6372aa68a81ac
陈少钦 <chenshaoqin@chenshaoqindeMacBook-Pro.local>
2016-12-03 23:36:22 +0800

    init attachment

---
+++ src/test/java/com/smarteshop/web/rest/AttachmentResourceIntTest.java
@@ -0,0 +1,330 @@
+package com.smarteshop.web.rest;
+
+import com.smarteshop.SmarteshopApp;
+
+import com.smarteshop.domain.Attachment;
+import com.smarteshop.repository.AttachmentRepository;
+import com.smarteshop.service.AttachmentService;
+import com.smarteshop.repository.search.AttachmentSearchRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Base64Utils;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the AttachmentResource REST controller.
+ *
+ * @see AttachmentResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SmarteshopApp.class)
+public class AttachmentResourceIntTest {
+
+    private static final String DEFAULT_ENTITY_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_ENTITY_NAME = "BBBBBBBBBB";
+
+    private static final Long DEFAULT_ENTITY_ID = 1L;
+    private static final Long UPDATED_ENTITY_ID = 2L;
+
+    private static final byte[] DEFAULT_CONTENT = TestUtil.createByteArray(1, "0");
+    private static final byte[] UPDATED_CONTENT = TestUtil.createByteArray(2, "1");
+    private static final String DEFAULT_CONTENT_CONTENT_TYPE = "image/jpg";
+    private static final String UPDATED_CONTENT_CONTENT_TYPE = "image/png";
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    @Inject
+    private AttachmentRepository attachmentRepository;
+
+    @Inject
+    private AttachmentService attachmentService;
+
+    @Inject
+    private AttachmentSearchRepository attachmentSearchRepository;
+
+    @Inject
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Inject
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Inject
+    private EntityManager em;
+
+    private MockMvc restAttachmentMockMvc;
+
+    private Attachment attachment;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        AttachmentResource attachmentResource = new AttachmentResource();
+        ReflectionTestUtils.setField(attachmentResource, "attachmentService", attachmentService);
+        this.restAttachmentMockMvc = MockMvcBuilders.standaloneSetup(attachmentResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Attachment createEntity(EntityManager em) {
+        Attachment attachment = new Attachment()
+                .entityName(DEFAULT_ENTITY_NAME)
+                .entityId(DEFAULT_ENTITY_ID)
+                .content(DEFAULT_CONTENT)
+                .contentContentType(DEFAULT_CONTENT_CONTENT_TYPE)
+                .name(DEFAULT_NAME);
+        return attachment;
+    }
+
+    @Before
+    public void initTest() {
+        attachmentSearchRepository.deleteAll();
+        attachment = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createAttachment() throws Exception {
+        int databaseSizeBeforeCreate = attachmentRepository.findAll().size();
+
+        // Create the Attachment
+
+        restAttachmentMockMvc.perform(post("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(attachment)))
+            .andExpect(status().isCreated());
+
+        // Validate the Attachment in the database
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeCreate + 1);
+        Attachment testAttachment = attachments.get(attachments.size() - 1);
+        assertThat(testAttachment.getEntityName()).isEqualTo(DEFAULT_ENTITY_NAME);
+        assertThat(testAttachment.getEntityId()).isEqualTo(DEFAULT_ENTITY_ID);
+        assertThat(testAttachment.getContent()).isEqualTo(DEFAULT_CONTENT);
+        assertThat(testAttachment.getContentContentType()).isEqualTo(DEFAULT_CONTENT_CONTENT_TYPE);
+        assertThat(testAttachment.getName()).isEqualTo(DEFAULT_NAME);
+
+        // Validate the Attachment in ElasticSearch
+        Attachment attachmentEs = attachmentSearchRepository.findOne(testAttachment.getId());
+        assertThat(attachmentEs).isEqualToComparingFieldByField(testAttachment);
+    }
+
+    @Test
+    @Transactional
+    public void checkEntityNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = attachmentRepository.findAll().size();
+        // set the field null
+        attachment.setEntityName(null);
+
+        // Create the Attachment, which fails.
+
+        restAttachmentMockMvc.perform(post("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(attachment)))
+            .andExpect(status().isBadRequest());
+
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkEntityIdIsRequired() throws Exception {
+        int databaseSizeBeforeTest = attachmentRepository.findAll().size();
+        // set the field null
+        attachment.setEntityId(null);
+
+        // Create the Attachment, which fails.
+
+        restAttachmentMockMvc.perform(post("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(attachment)))
+            .andExpect(status().isBadRequest());
+
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkContentIsRequired() throws Exception {
+        int databaseSizeBeforeTest = attachmentRepository.findAll().size();
+        // set the field null
+        attachment.setContent(null);
+
+        // Create the Attachment, which fails.
+
+        restAttachmentMockMvc.perform(post("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(attachment)))
+            .andExpect(status().isBadRequest());
+
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = attachmentRepository.findAll().size();
+        // set the field null
+        attachment.setName(null);
+
+        // Create the Attachment, which fails.
+
+        restAttachmentMockMvc.perform(post("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(attachment)))
+            .andExpect(status().isBadRequest());
+
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllAttachments() throws Exception {
+        // Initialize the database
+        attachmentRepository.saveAndFlush(attachment);
+
+        // Get all the attachments
+        restAttachmentMockMvc.perform(get("/api/attachments?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(attachment.getId().intValue())))
+            .andExpect(jsonPath("$.[*].entityName").value(hasItem(DEFAULT_ENTITY_NAME.toString())))
+            .andExpect(jsonPath("$.[*].entityId").value(hasItem(DEFAULT_ENTITY_ID.intValue())))
+            .andExpect(jsonPath("$.[*].contentContentType").value(hasItem(DEFAULT_CONTENT_CONTENT_TYPE)))
+            .andExpect(jsonPath("$.[*].content").value(hasItem(Base64Utils.encodeToString(DEFAULT_CONTENT))))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getAttachment() throws Exception {
+        // Initialize the database
+        attachmentRepository.saveAndFlush(attachment);
+
+        // Get the attachment
+        restAttachmentMockMvc.perform(get("/api/attachments/{id}", attachment.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(attachment.getId().intValue()))
+            .andExpect(jsonPath("$.entityName").value(DEFAULT_ENTITY_NAME.toString()))
+            .andExpect(jsonPath("$.entityId").value(DEFAULT_ENTITY_ID.intValue()))
+            .andExpect(jsonPath("$.contentContentType").value(DEFAULT_CONTENT_CONTENT_TYPE))
+            .andExpect(jsonPath("$.content").value(Base64Utils.encodeToString(DEFAULT_CONTENT)))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingAttachment() throws Exception {
+        // Get the attachment
+        restAttachmentMockMvc.perform(get("/api/attachments/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateAttachment() throws Exception {
+        // Initialize the database
+        attachmentService.save(attachment);
+
+        int databaseSizeBeforeUpdate = attachmentRepository.findAll().size();
+
+        // Update the attachment
+        Attachment updatedAttachment = attachmentRepository.findOne(attachment.getId());
+        updatedAttachment
+                .entityName(UPDATED_ENTITY_NAME)
+                .entityId(UPDATED_ENTITY_ID)
+                .content(UPDATED_CONTENT)
+                .contentContentType(UPDATED_CONTENT_CONTENT_TYPE)
+                .name(UPDATED_NAME);
+
+        restAttachmentMockMvc.perform(put("/api/attachments")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedAttachment)))
+            .andExpect(status().isOk());
+
+        // Validate the Attachment in the database
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeUpdate);
+        Attachment testAttachment = attachments.get(attachments.size() - 1);
+        assertThat(testAttachment.getEntityName()).isEqualTo(UPDATED_ENTITY_NAME);
+        assertThat(testAttachment.getEntityId()).isEqualTo(UPDATED_ENTITY_ID);
+        assertThat(testAttachment.getContent()).isEqualTo(UPDATED_CONTENT);
+        assertThat(testAttachment.getContentContentType()).isEqualTo(UPDATED_CONTENT_CONTENT_TYPE);
+        assertThat(testAttachment.getName()).isEqualTo(UPDATED_NAME);
+
+        // Validate the Attachment in ElasticSearch
+        Attachment attachmentEs = attachmentSearchRepository.findOne(testAttachment.getId());
+        assertThat(attachmentEs).isEqualToComparingFieldByField(testAttachment);
+    }
+
+    @Test
+    @Transactional
+    public void deleteAttachment() throws Exception {
+        // Initialize the database
+        attachmentService.save(attachment);
+
+        int databaseSizeBeforeDelete = attachmentRepository.findAll().size();
+
+        // Get the attachment
+        restAttachmentMockMvc.perform(delete("/api/attachments/{id}", attachment.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate ElasticSearch is empty
+        boolean attachmentExistsInEs = attachmentSearchRepository.exists(attachment.getId());
+        assertThat(attachmentExistsInEs).isFalse();
+
+        // Validate the database is empty
+        List<Attachment> attachments = attachmentRepository.findAll();
+        assertThat(attachments).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void searchAttachment() throws Exception {
+        // Initialize the database
+        attachmentService.save(attachment);
+
+        // Search the attachment
+        restAttachmentMockMvc.perform(get("/api/_search/attachments?query=id:" + attachment.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(attachment.getId().intValue())))
+            .andExpect(jsonPath("$.[*].entityName").value(hasItem(DEFAULT_ENTITY_NAME.toString())))
+            .andExpect(jsonPath("$.[*].entityId").value(hasItem(DEFAULT_ENTITY_ID.intValue())))
+            .andExpect(jsonPath("$.[*].contentContentType").value(hasItem(DEFAULT_CONTENT_CONTENT_TYPE)))
+            .andExpect(jsonPath("$.[*].content").value(hasItem(Base64Utils.encodeToString(DEFAULT_CONTENT))))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())));
+    }
+}
