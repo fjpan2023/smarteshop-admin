6c0df0421ce3f999a2f9fb2b4e92b1d9ae14bbaf
陈少钦 <chenshaoqin@chenshaoqindeMacBook-Pro.local>
2016-12-05 23:39:50 +0800

    init ...

--- src/main/webapp/app/entities/brand/brand-detail.controller.js
+++ src/main/webapp/app/entities/brand/brand-detail.controller.js
@@ -5,15 +5,22 @@
         .module('smarteshopApp')
         .controller('BrandDetailController', BrandDetailController);
 
-    BrandDetailController.$inject = ['$scope','$rootScope', '$stateParams', 'previousState', 'DataUtils', 'entity', 'Brand'];
+    BrandDetailController.$inject = ['$scope','$rootScope', '$stateParams', 'previousState', 'DataUtils', 'entity', 'Brand','Attachment'];
 
-    function BrandDetailController($scope,$rootScope, $stateParams, previousState, DataUtils, entity, Brand) {
+    function BrandDetailController($scope,$rootScope, $stateParams, previousState, DataUtils, entity, Brand, Attachment) {
         var vm = this;
 
         vm.brand = entity;
         vm.previousState = previousState.name;
         vm.byteSize = DataUtils.byteSize;
         vm.openFile = DataUtils.openFile;
+        Attachment.query({entityId:entity.id, entityName:'Brand'}, function(attachments){
+        	if(attachments){
+        		vm.image = attachments[0];
+        	}
+        	
+        });
+        
 
         var unsubscribe = $rootScope.$on('smarteshopApp:brandUpdate', function(event, result) {
             vm.brand = result;
