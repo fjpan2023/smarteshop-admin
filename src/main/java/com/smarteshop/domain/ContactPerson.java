707ad4c6de715d980d29a8c85fb8f5482cb99365
陈少钦 <chenshaoqin@chenshaoqindeMacBook-Pro.local>
2016-12-20 08:04:40 +0800

    init ...

--- src/main/java/com/smarteshop/domain/ContactPerson.java
+++ src/main/java/com/smarteshop/domain/ContactPerson.java
@@ -29,6 +29,13 @@ public class ContactPerson extends AbstractAuditingEntity implements Serializabl
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     @Column(name = "name")
     private String name;
 
@@ -105,13 +112,27 @@ public class ContactPerson extends AbstractAuditingEntity implements Serializabl
         return Objects.hashCode(id);
     }
 
+    public String getFirstName() {
+      return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+      this.firstName = firstName;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public void setLastName(String lastName) {
+      this.lastName = lastName;
+    }
+
     @Override
     public String toString() {
-        return "ContactPerson{" +
-            "id=" + id +
-            ", name='" + name + "'" +
-            ", email='" + email + "'" +
-            ", phone='" + phone + "'" +
-            '}';
+      return "ContactPerson [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+          + ", name=" + name + ", email=" + email + ", phone=" + phone + "]";
     }
+
+
 }
