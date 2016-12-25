780c60576be8165ec772baaa73576e9e17d23a71
陈少钦 <chenshaoqin@chenshaqindeMBP.dhcp.pvgl.sap.corp>
2016-12-25 12:11:10 +0800

    init ....

--- src/main/java/com/smarteshop/web/rest/EmailSettingController.java
+++ src/main/java/com/smarteshop/web/rest/EmailSettingController.java
@@ -1,6 +1,5 @@
 package com.smarteshop.web.rest;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.inject.Inject;
@@ -12,7 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codahale.metrics.annotation.Timed;
 import com.smarteshop.domain.EmailSetting;
+import com.smarteshop.exception.BusinessException;
 import com.smarteshop.service.EmailSettingService;
 import com.smarteshop.web.common.AbstractController;
 import com.smarteshop.web.rest.util.HeaderUtil;
@@ -35,27 +34,6 @@ public class EmailSettingController extends AbstractController<EmailSetting> {
 
     @Inject
     private EmailSettingService emailSettingService;
-
-    /**
-     * POST  /email-settings : Create a new emailSetting.
-     *
-     * @param emailSetting the emailSetting to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new emailSetting, or with status 400 (Bad Request) if the emailSetting has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("")
-    @Timed
-    public ResponseEntity<EmailSetting> createEmailSetting(@Valid @RequestBody EmailSetting emailSetting) throws URISyntaxException {
-        log.debug("REST request to save EmailSetting : {}", emailSetting);
-        if (emailSetting.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("emailSetting", "idexists", "A new emailSetting cannot already have an ID")).body(null);
-        }
-        EmailSetting result = emailSettingService.save(emailSetting);
-        return ResponseEntity.created(new URI("/api/email-settings/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert("emailSetting", result.getId().toString()))
-            .body(result);
-    }
-
     /**
      * PUT  /email-settings : Updates an existing emailSetting.
      *
@@ -70,7 +48,7 @@ public class EmailSettingController extends AbstractController<EmailSetting> {
     public ResponseEntity<EmailSetting> updateEmailSetting(@Valid @RequestBody EmailSetting emailSetting) throws URISyntaxException {
         log.debug("REST request to update EmailSetting : {}", emailSetting);
         if (emailSetting.getId() == null) {
-            return createEmailSetting(emailSetting);
+            throw new BusinessException("id is null ");
         }
         EmailSetting result = emailSettingService.save(emailSetting);
         return ResponseEntity.ok()
--- src/main/webapp/app/entities/email-setting/email-setting.state.js
+++ src/main/webapp/app/entities/email-setting/email-setting.state.js
@@ -9,52 +9,9 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-        .state('email-setting', {
-            parent: 'entity',
-            url: '/email-setting?page&sort&search',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'smarteshopApp.emailSetting.home.title'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/email-setting/email-settings.html',
-                    controller: 'EmailSettingController',
-                    controllerAs: 'vm'
-                }
-            },
-            params: {
-                page: {
-                    value: '1',
-                    squash: true
-                },
-                sort: {
-                    value: 'id,asc',
-                    squash: true
-                },
-                search: null
-            },
-            resolve: {
-                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
-                    return {
-                        page: PaginationUtil.parsePage($stateParams.page),
-                        sort: $stateParams.sort,
-                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
-                        ascending: PaginationUtil.parseAscending($stateParams.sort),
-                        search: $stateParams.search
-                    };
-                }],
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
-                    $translatePartialLoader.addPart('emailSetting');
-                    $translatePartialLoader.addPart('sMTPSecurityEnum');
-                    $translatePartialLoader.addPart('global');
-                    return $translate.refresh();
-                }]
-            }
-        })
         .state('email-setting-detail', {
             parent: 'entity',
-            url: '/email-setting/{id}',
+            url: '/emailSetting',
             data: {
                 authorities: ['ROLE_USER'],
                 pageTitle: 'smarteshopApp.emailSetting.detail.title'
@@ -110,39 +67,6 @@
                 });
             }]
         })
-        .state('email-setting.new', {
-            parent: 'email-setting',
-            url: '/new',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/email-setting/email-setting-dialog.html',
-                    controller: 'EmailSettingDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: function () {
-                            return {
-                                host: null,
-                                port: null,
-                                smtpSecurity: null,
-                                fromAddress: null,
-                                userName: null,
-                                password: null,
-                                id: null
-                            };
-                        }
-                    }
-                }).result.then(function() {
-                    $state.go('email-setting', null, { reload: 'email-setting' });
-                }, function() {
-                    $state.go('email-setting');
-                });
-            }]
-        })
         .state('email-setting.edit', {
             parent: 'email-setting',
             url: '/{id}/edit',
@@ -156,30 +80,6 @@
                     controllerAs: 'vm',
                     backdrop: 'static',
                     size: 'lg',
-                    resolve: {
-                        entity: ['EmailSetting', function(EmailSetting) {
-                            return EmailSetting.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('email-setting', null, { reload: 'email-setting' });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('email-setting.delete', {
-            parent: 'email-setting',
-            url: '/{id}/delete',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/email-setting/email-setting-delete-dialog.html',
-                    controller: 'EmailSettingDeleteController',
-                    controllerAs: 'vm',
-                    size: 'md',
                     resolve: {
                         entity: ['EmailSetting', function(EmailSetting) {
                             return EmailSetting.get({id : $stateParams.id}).$promise;
